27550deef83a70c84b9af4e6e1d4aaa93b65462d
Ramon Tayag <ramon@tayag.net>
2011-10-22 14:43:30 +0800

    Whitespace cleanup

--- spec/adapters/postgresql_adapter_spec.rb
+++ spec/adapters/postgresql_adapter_spec.rb
@@ -2,49 +2,49 @@ require 'spec_helper'
 require 'apartment/adapters/postgresql_adapter'   # specific adapters get dynamically loaded based on adapter name, so we must manually require here
 
 describe Apartment::Adapters::PostgresqlAdapter do
-  
+
   before do
     ActiveRecord::Base.establish_connection Apartment::Test.config['connections']['postgresql']
     @schema_search_path = ActiveRecord::Base.connection.schema_search_path
   end
-  
+
   after do
     ActiveRecord::Base.clear_all_connections!
   end
-  
+
   context "using schemas" do
-    
+
     let(:schema){ 'first_db_schema' }
     let(:schema2){ 'another_db_schema' }
     let(:database_names){ ActiveRecord::Base.connection.execute("SELECT nspname FROM pg_namespace;").collect{|row| row['nspname']} }
-    
+
     subject{ Apartment::Database.postgresql_adapter Apartment::Test.config['connections']['postgresql'].symbolize_keys }
-  
+
     before do
       Apartment.use_postgres_schemas = true
       subject.create(schema)
     end
-  
+
     after do
       Apartment::Test.drop_schema(schema)
     end
-    
+
     describe "#create" do
-      
+
       it "should create the new schema" do
         database_names.should include(schema)
       end
-    
+
       it "should load schema.rb to new schema" do
         ActiveRecord::Base.connection.schema_search_path = schema
         ActiveRecord::Base.connection.tables.should include('companies')
       end
-    
+
       it "should reset connection when finished" do
         ActiveRecord::Base.connection.schema_search_path.should_not == schema
       end
     end
-    
+
     describe "#drop" do
 
       it "should delete the database" do
@@ -61,21 +61,21 @@ describe Apartment::Adapters::PostgresqlAdapter do
         }.to raise_error(Apartment::SchemaNotFound)
       end
     end
-    
-    
+
+
     describe "#process" do
       it "should connect" do
         subject.process(schema) do
           ActiveRecord::Base.connection.schema_search_path.should == schema
         end
       end
-      
+
       it "should reset" do
         subject.process(schema)
         ActiveRecord::Base.connection.schema_search_path.should == @schema_search_path
       end
     end
-    
+
     describe "#reset" do
       it "should reset connection" do
         subject.switch(schema)
@@ -83,28 +83,28 @@ describe Apartment::Adapters::PostgresqlAdapter do
         ActiveRecord::Base.connection.schema_search_path.should == @schema_search_path
       end
     end
-    
+
     describe "#switch" do
       it "should connect to new schema" do
         subject.switch(schema)
         ActiveRecord::Base.connection.schema_search_path.should == schema
       end
-      
+
       it "should reset connection if database is nil" do
         subject.switch
         ActiveRecord::Base.connection.schema_search_path.should == @schema_search_path
       end
     end
-    
+
     describe "#current_database" do
       it "should return the current schema name" do
         subject.switch(schema)
         subject.current_database.should == schema
       end
     end
-    
+
   end
-  
+
   context "using databases" do
     # TODO
   end
