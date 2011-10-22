1e872293e5826be21179489b5617c831c0303567
Ramon Tayag <ramon@tayag.net>
2011-10-22 13:34:49 +0800

    Cleans up tabs and whitespace

--- lib/apartment/adapters/postgresql_adapter.rb
+++ lib/apartment/adapters/postgresql_adapter.rb
@@ -1,114 +1,114 @@
 module Apartment
-  
+
   module Database
-    
+
     def self.postgresql_adapter(config)
-      Apartment.use_postgres_schemas ? 
+      Apartment.use_postgres_schemas ?
         Adapters::PostgresqlSchemaAdapter.new(config, :schema_search_path => ActiveRecord::Base.connection.schema_search_path) :
         Adapters::PostgresqlAdapter.new(config)
     end
-    
+
   end
-  
+
   module Adapters
-    
+
     # Default adapter when not using Postgresql Schemas
     class PostgresqlAdapter < AbstractAdapter
-      
-    protected
-    
+
+      protected
+
       #   Connect to new database
       #   Abstract adapter will catch generic ActiveRecord error
       #   Catch specific adapter errors here
-      # 
+      #
       #   @param {String} database Database name
-      # 
+      #
       def connect_to_new(database)
         super
       rescue PGError => e
         raise DatabaseNotFound, "Cannot find database #{environmentify(database)}"
-		  end
-		  
+      end
+
     end
-    
+
     # Separate Adapter for Postgresql when using schemas
     class PostgresqlSchemaAdapter < AbstractAdapter
-      
+
       #   Get the current schema search path
-      # 
+      #
       #   @return {String} current schema search path
-      # 
+      #
       def current_database
         ActiveRecord::Base.connection.schema_search_path
       end
-      
+
       #   Drop the database schema
-      # 
+      #
       #   @param {String} database Database (schema) to drop
-      # 
+      #
       def drop(database)
         ActiveRecord::Base.connection.execute("DROP SCHEMA #{database} CASCADE")
-        
+
       rescue ActiveRecord::StatementInvalid => e
         raise SchemaNotFound, "The schema #{database.inspect} cannot be found."
       end
-  	  
+
       #   Reset search path to default search_path
       #   Set the table_name to always use the public namespace for excluded models
-      # 
+      #
       def process_excluded_models
-  	    Apartment.excluded_models.each do |excluded_model|
+        Apartment.excluded_models.each do |excluded_model|
           # Note that due to rails reloading, we now take string references to classes rather than
           # actual object references.  This way when we contantize, we always get the proper class reference
           if excluded_model.is_a? Class
             warn "[Deprecation Warning] Passing class references to excluded models is now deprecated, please use a string instead"
             excluded_model = excluded_model.name
           end
-          
+
           excluded_model.constantize.tap do |klass|
-            # some models (such as delayed_job) seem to load and cache their column names before this, 
+            # some models (such as delayed_job) seem to load and cache their column names before this,
             # so would never get the public prefix, so reset first
-    	      klass.reset_column_information
+            klass.reset_column_information
 
             # Ensure that if a schema *was* set, we override
-      	    table_name = klass.table_name.split('.', 2).last
+            table_name = klass.table_name.split('.', 2).last
 
             # Not sure why, but Delayed::Job somehow ignores table_name_prefix...  so we'll just manually set table name instead
-    				klass.table_name = "public.#{table_name}"
-  				end
-  			end
+            klass.table_name = "public.#{table_name}"
+          end
+        end
       end
-  	  
+
       #   Reset schema search path to the default schema_search_path
-      # 
+      #
       #   @return {String} default schema search path
-      # 
-			def reset
-    		ActiveRecord::Base.connection.schema_search_path = @defaults[:schema_search_path]
-  	  end
-  	  
-  	protected
-  	
-  	  #   Set schema search path to new schema
-      # 
-	    def connect_to_new(database = nil)
-	      return reset if database.nil?
-    		ActiveRecord::Base.connection.schema_search_path = database
-    		
+      #
+      def reset
+        ActiveRecord::Base.connection.schema_search_path = @defaults[:schema_search_path]
+      end
+
+      protected
+
+      #   Set schema search path to new schema
+      #
+      def connect_to_new(database = nil)
+        return reset if database.nil?
+        ActiveRecord::Base.connection.schema_search_path = database
+
       rescue ActiveRecord::StatementInvalid => e
         raise SchemaNotFound, "The schema #{database.inspect} cannot be found."
-			end
-  	  
-      #   Create the new schema
-      # 
-  	  def create_database(database)
-  	    ActiveRecord::Base.connection.execute("CREATE SCHEMA #{database}")
-  	    
-  	  rescue ActiveRecord::StatementInvalid => e
-  		  raise SchemaExists, "The schema #{database} already exists."
       end
-      
+
+      #   Create the new schema
+      #
+      def create_database(database)
+        ActiveRecord::Base.connection.execute("CREATE SCHEMA #{database}")
+
+      rescue ActiveRecord::StatementInvalid => e
+        raise SchemaExists, "The schema #{database} already exists."
+      end
+
     end
-    
+
   end
 end
--- lib/apartment/railtie.rb
+++ lib/apartment/railtie.rb
@@ -1,12 +1,12 @@
 require 'rails'
 
 module Apartment
-	class Railtie < Rails::Railtie
-	  
+  class Railtie < Rails::Railtie
+
     # Set up our default config options
     # Do this before the app initializers run so we don't override custom settings
-	  config.before_initialize do
-	    Apartment.configure do |config|
+    config.before_initialize do
+      Apartment.configure do |config|
         config.excluded_models = []
         config.use_postgres_schemas = true
         config.database_names = []
@@ -14,33 +14,33 @@ module Apartment
         config.prepend_environment = true
       end
     end
-    
+
     #   Hook into ActionDispatch::Reloader to ensure Apartment is properly initialized
     #   Note that this doens't entirely work as expected in Development, because this is called before classes are reloaded
     #   See the above middleware/console declarations below to help with this.  Hope to fix that soon.
     config.to_prepare do
       Apartment::Database.init
     end
-	  
-		rake_tasks do
-			load 'tasks/apartment.rake'
-		end
-		
+
+    rake_tasks do
+      load 'tasks/apartment.rake'
+    end
+
     # The following initializers are a workaround to the fact that I can't properly hook into the rails reloader
     # Note this is technically valid for any environment where cache_classes is false, for us, it's just development
     if Rails.env.development?
-      
+
       # Apartment::Reloader is middleware to initialize things properly on each request to dev
-  		initializer 'apartment.init' do |app|
+      initializer 'apartment.init' do |app|
         app.config.middleware.use "Apartment::Reloader"
       end
-    
+
       # Overrides reload! to also call Apartment::Database.init as well so that the reloaded classes have the proper table_names
       console do
         require 'apartment/console'
       end
-      
+
     end
-    
-	end
+
+  end
 end
